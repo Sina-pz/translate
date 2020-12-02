83bb1d5ab1189065e428eb74cfe31133324f3a4d
Sina Pashazanous <sina.pashazanous@ntint.com>
2020-12-02 15:25:54 -0500

    app erro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { SettingBoxComponent } from './test-modules/setting-box/setting-box.component';
 import { ClocksSettingComponent } from './test-modules/clocks-setting/clocks-setting.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LanguageComponent } from './test-modules/language-i18n/language.component';
 import { RightToLeftComponent } from './right-to-left/right-to-left.component';
@@ -20,7 +20,6 @@ import { DxTextBoxModule } from 'devextreme-angular';
 import { NvLanguageComponent } from './shared/nv-language/nv-language.component';
 import { TextBoxRtlComponent } from './text-box-rtl/text-box-rtl.component';
 import { DatePipeComponent } from './test-modules/date-pipe/date-pipe.component';
-import { LtrDevExtremeComponent } from './test-modules/ltr-dev-extreme/ltr-dev-extreme.component';
 import { AnalogClockComponent } from './test-modules/analog-clock/analog-clock.component';
 import { DigitalClockComponent } from './test-modules/digital-clock/digital-clock.component';
 
@@ -29,14 +28,13 @@ import { DigitalClockComponent } from './test-modules/digital-clock/digital-cloc
     AppComponent,
     LanguageComponent,
     RightToLeftComponent,
-    LtrDevExtremeComponent,
     NvLanguageComponent,
     TextBoxRtlComponent,
     DatePipeComponent,
     AnalogClockComponent,
     DigitalClockComponent,
     ClocksSettingComponent,
-    SettingBoxComponent
+    SettingBoxComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,6 +47,8 @@ import { DigitalClockComponent } from './test-modules/digital-clock/digital-cloc
     BrowserModule,
     DxSelectBoxModule,
     DxTextBoxModule,
+    NgModule,
+    FormsModule,
 
   ],
   providers: [],
